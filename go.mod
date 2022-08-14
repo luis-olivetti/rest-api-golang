e333834f9149cd1e4812f99087c8051aa46dda3d
luis-olivetti <luuisf92@gmail.com>
2022-08-14 16:26:47 -0300

    Adicionado leitura do banco Sqlite3

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/luis-olivetti/rest-api-golang
 go 1.17
 
 require github.com/gorilla/mux v1.8.0
+
+require github.com/mattn/go-sqlite3 v1.14.14
--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 var people []Person
@@ -15,8 +18,35 @@ func getPeople(w http.ResponseWriter, r *http.Request) {
 }
 
 func populatePeople() {
-	people = append(people, Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}})
-	people = append(people, Person{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &Address{City: "City Z", State: "State Y"}})
+
+	db, err := sql.Open("sqlite3", "./godb")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT Id, FirstName, LastName FROM People")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var id int
+		var firstName, lastName string
+
+		err = rows.Scan(&id, &firstName, &lastName)
+		if err != nil {
+			log.Fatal(err)
+		}
+		people = append(people, Person{ID: strconv.Itoa(id), Firstname: firstName, Lastname: lastName})
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//people = append(people, Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}})
+	//people = append(people, Person{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &Address{City: "City Z", State: "State Y"}})
 }
 
 func main() {
